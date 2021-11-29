4c6b5b13f73f8438f821b273c48f83582b270228
Adhil-M <adhilmankarathodi@gmail.com>
2021-11-29 11:54:11 +0530

    d9

--- src/main/java/com/dbms/placementapp/controllers/UserController.java
+++ src/main/java/com/dbms/placementapp/controllers/UserController.java
@@ -25,6 +25,7 @@ import com.dbms.placementapp.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/user")
 public class UserController {
     @Autowired
